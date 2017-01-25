32d76f1ca1df0fec5c34e775abc51aa18508b09b
farez <f4arez@gmail.com>
2017-01-25 19:54:52 +0100

    Added som ntested artnet

--- main.go
+++ main.go
@@ -4,27 +4,57 @@ import (
 	"github.com/gordonklaus/portaudio"
 	//"log"
 	"math"
+	"src/DMXReciever"
 	"time"
 )
+
+/*
+
+		18000	18064	18128	18192	18256	18320	18384	18448	18512	18576	18640	18704	18768	18832	18896	18960
+18000		0		1		2		3		4		5		6		7		8		9		10		11		12		13		14		15
+18064		16		17		18		19		20		21		22		23		24		25		26		27		28		29		30		31
+18192
+18256
+18320
+18384
+18448
+18512
+18576
+18640
+18704
+18768
+18832
+18896
+18960
+
+
+*/
 
 const sampleRate = 44100
 
 func main() {
+	dmxChan := make(chan DMXReciever.DmxSignal)
+	go DMXReciever.ReadArtnet(dmxChan)
+
 	portaudio.Initialize()
 	defer portaudio.Terminate()
 
 	sleepTime := 50 * time.Millisecond
 	for {
-		r := newStereoSine(18000, 18256, sampleRate)
-
+		dmx := <-dmxChan
+		first, second := makeColorHz(18000, dmx.R)
+		r := newStereoSine(first, second, sampleRate)
 		chk(r.Start())
 		time.Sleep(sleepTime)
 		chk(r.Stop())
-		g := newStereoSine(19000, 19256, sampleRate)
+		first, second = makeColorHz(19000, dmx.G)
+		g := newStereoSine(first, second, sampleRate)
 		chk(g.Start())
 		time.Sleep(sleepTime)
 		chk(g.Stop())
-		b := newStereoSine(20000, 20256, sampleRate)
+		first, second = makeColorHz(20000, dmx.B)
+
+		b := newStereoSine(first, second, sampleRate)
 
 		chk(b.Start())
 		time.Sleep(sleepTime)
@@ -58,7 +88,6 @@ func (g *stereoSine) processAudio(out [][]float32) {
 	for i := range out[0] {
 		if !sL || lP < numberOfPhases {
 			out[0][i] = float32(math.Sin(2*math.Pi*g.phaseL)) * curveFunc(lP)
-			//log.Println(out[0][i], lP, curveFunc(lP))
 			p, g.phaseL = math.Modf(g.phaseL + g.stepL)
 			if p == 1 {
 				lP++
@@ -85,6 +114,13 @@ func (g *stereoSine) processAudio(out [][]float32) {
 	}
 }
 
+func makeColorHz(start int, color int) (int, int) {
+	first := color % 16
+	second := color / 16
+	first = start + first*64
+	second = start + 1000 + second*64
+}
+
 func chk(err error) {
 	if err != nil {
 		panic(err)
@@ -101,8 +137,3 @@ const (
 func curveFunc(x float64) float32 {
 	return float32(a * math.Exp(-math.Pow(x-b, 2)/2.0*math.Pow(c, 2)))
 }
-
-/*if g.phaseR < 0.009 && g.phaseL < 0.009 {
-	log.Println(g.phaseL, g.phaseR)
-	break
-}*/
